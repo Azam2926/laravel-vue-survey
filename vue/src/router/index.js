263872c45e082113456b3806d5b7b6325be845b0
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-27 08:50:40 +0400

    Working on youtube recording

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -17,7 +17,8 @@ const routes = [
     children: [
       { path: "/dashboard", name: "Dashboard", component: Dashboard },
       { path: "/surveys", name: "Surveys", component: Surveys },
-      { path: "/survey/:id", name: "SurveyView", component: SurveyView },
+      { path: "/surveys/create", name: "SurveyCreate", component: SurveyView },
+      { path: "/surveys/:id", name: "SurveyView", component: SurveyView },
     ],
   },
   {
