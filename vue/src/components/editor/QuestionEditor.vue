2a45c7b5c1f69af48b7199dcaa9b17b09bddc521
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-28 08:56:59 +0400

    Almost finish QuestionEditor component

--- vue/src/components/editor/QuestionEditor.vue
+++ vue/src/components/editor/QuestionEditor.vue
@@ -1,10 +1,7 @@
 <template>
   <!-- Question index -->
   <div class="flex items-center justify-between">
-    <h3 class="text-lg font-bold">
-      {{ index + 1 }}. {{ model.question }}
-    </h3>
-
+    <h3 class="text-lg font-bold">{{ index + 1 }}. {{ model.question }}</h3>
 
     <div class="flex items-center">
       <!-- Add new question -->
@@ -90,8 +87,7 @@
         :id="'question_text_' + model.data"
         class="
           mt-1
-          focus:ring-indigo-500
-          focus:border-indigo-500
+          focus:ring-indigo-500 focus:border-indigo-500
           block
           w-full
           shadow-sm
@@ -123,9 +119,7 @@
           bg-white
           rounded-md
           shadow-sm
-          focus:outline-none
-          focus:ring-indigo-500
-          focus:border-indigo-500
+          focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
           sm:text-sm
         "
       >
@@ -151,8 +145,7 @@
       :id="'question_description_' + model.id"
       class="
         mt-1
-        focus:ring-indigo-500
-        focus:border-indigo-500
+        focus:ring-indigo-500 focus:border-indigo-500
         block
         w-full
         shadow-sm
@@ -165,14 +158,114 @@
   <!--/ Question Description -->
 
   <!-- Data -->
-  Work here
+  <div>
+    <div v-if="shouldHaveOptions()" class="mt-2">
+      <h4 class="text-sm font-semibold mb-1 flex justify-between items-center">
+        Options
+
+        <!-- Add new option -->
+        <button
+          type="button"
+          @click="addOption()"
+          class="
+            flex
+            items-center
+            text-xs
+            py-1
+            px-2
+            rounded-sm
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Add Option
+        </button>
+        <!--/ Add new option -->
+      </h4>
+
+      <div
+        v-if="!model.data.options.length"
+        class="text-xs text-gray-600 text-center py-3"
+      >
+        You don't have any options defined
+      </div>
+      <!-- Option list -->
+      <div
+        v-for="(option, index) in model.data.options"
+        :key="option.uuid"
+        class="flex items-center mb-1"
+      >
+        <span class="w-6 text-sm"> {{ index + 1 }}. </span>
+        <input
+          type="text"
+          v-model="option.text"
+          @change="dataChange"
+          class="
+            w-full
+            rounded-sm
+            py-1
+            px-2
+            text-xs
+            border border-gray-300
+            focus:border-indigo-500
+          "
+        />
+        <!-- Delete Option -->
+        <button
+          type="button"
+          @click="removeOption(option)"
+          class="
+            h-6
+            w-6
+            rounded-full
+            flex
+            items-center
+            justify-center
+            border border-transparent
+            transition-colors
+            hover:border-red-100
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-3 w-3 text-red-500"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <!--/ Delete Option -->
+      </div>
+      <!--/ Option list -->
+    </div>
+  </div>
   <!--/ Data -->
 
   <hr class="my-4" />
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { v4 as uuidv4 } from "uuid";
+import store from "../../store";
 
 const props = defineProps({
   question: Object,
@@ -183,6 +276,59 @@ const emit = defineEmits(["change", "addQuestion", "deleteQuestion"]);
 
 // Re-create the whole question data to avoid unintentional reference change
 const model = ref(JSON.parse(JSON.stringify(props.question)));
+
+
+// Get question types from vuex
+const questionTypes = computed(() => store.state.questionTypes);
+
+function upperCaseFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Check if the question should have options
+function shouldHaveOptions() {
+  return ["select", "radio", "checkbox"].includes(model.value.type);
+}
+
+function getOptions() {
+  return model.value.data.options;
+}
+
+function setOptions(options) {
+  model.value.data.options = options;
+}
+
+// Add option
+function addOption() {
+  setOptions([
+    ...getOptions(),
+    { uuid: uuidv4(), text: "" },
+  ]);
+  dataChange();
+}
+
+// Remove option
+function removeOption(op) {
+  setOptions(getOptions().filter((opt) => opt !== op));
+  dataChange();
+}
+
+function typeChange() {
+  if (shouldHaveOptions()) {
+    setOptions(getOptions() || []);
+  }
+  dataChange();
+}
+
+// Emit the data change
+function dataChange() {
+  const data = JSON.parse(JSON.stringify(model.value));
+  if (!shouldHaveOptions()) {
+    delete data.data.options;
+  }
+
+  emit("change", data);
+}
 
 </script>
 
