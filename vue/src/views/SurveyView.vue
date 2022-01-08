afb26c9d8e0ff6866d10d784274c885de92a6543
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-08 10:29:32 +0400

    Show Loading text on survey loading

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -3,11 +3,12 @@
     <template v-slot:header>
       <div class="flex items-center justify-between">
         <h1 class="text-3xl font-bold text-gray-900">
-          {{ model.id ? model.title : "Create a Survey" }}
+          {{ route.params.id ? model.title : "Create a Survey" }}
         </h1>
       </div>
     </template>
-    <form @submit.prevent="saveSurvey">
+    <div v-if="surveyLoading" class="flex justify-center">Loading...</div>
+    <form v-else @submit.prevent="saveSurvey">
       <div class="shadow sm:rounded-md sm:overflow-hidden">
         <!-- Survey Fields -->
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
@@ -288,7 +289,7 @@
 <script setup>
 import { v4 as uuidv4 } from "uuid";
 import store from "../store";
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import PageComponent from "../components/PageComponent.vue";
@@ -297,6 +298,8 @@ import QuestionEditor from "../components/editor/QuestionEditor.vue";
 const router = useRouter()
 
 const route = useRoute();
+
+const surveyLoading = computed(() => store.state.currentSurvey.loading)
 
 // Create empty survey
 let model = ref({
