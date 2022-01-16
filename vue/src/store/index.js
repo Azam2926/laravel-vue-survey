6255ac88b1c344c9a0f0db88f67179eaf92291d2
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 14:47:19 +0400

    Create getUser action and update setUser mutation

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -109,14 +109,16 @@ const store = createStore({
     register({commit}, user) {
       return axiosClient.post('/register', user)
         .then(({data}) => {
-          commit('setUser', data);
+          commit('setUser', data.user);
+          commit('setToken', data.token)
           return data;
         })
     },
     login({commit}, user) {
       return axiosClient.post('/login', user)
         .then(({data}) => {
-          commit('setUser', data);
+          commit('setUser', data.user);
+          commit('setToken', data.token)
           return data;
         })
     },
@@ -126,6 +128,13 @@ const store = createStore({
           commit('logout')
           return response;
         })
+    },
+    getUser({commit}) {
+      return axiosClient.get('/user')
+      .then(res => {
+        console.log(res);
+        commit('setUser', res.data)
+      })
     }
   },
   mutations: {
@@ -153,10 +162,12 @@ const store = createStore({
       state.user.data = {};
       sessionStorage.removeItem('TOKEN');
     },
-    setUser: (state, userData) => {
-      state.user.token = userData.token;
-      state.user.data = userData.user;
-      sessionStorage.setItem('TOKEN', userData.token);
+    setUser: (state, user) => {
+      state.user.data = user;
+    },
+    setToken: (state, token) => {
+      state.user.token = token;
+      sessionStorage.setItem('TOKEN', token);
     },
     notify: (state, {message, type}) => {
       state.notification.show = true;
