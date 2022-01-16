8163181014dcff2a141527d585827e0ad395fe49
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 14:49:43 +0400

    Implement Not Found for survey public page

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -5,27 +5,49 @@
         <h1 class="text-3xl font-bold text-gray-900">
           {{ route.params.id ? model.title : "Create a Survey" }}
         </h1>
-
-        <button
-          v-if="route.params.id"
-          type="button"
-          @click="deleteSurvey()"
-          class="py-2 px-3 text-white bg-red-500 rounded-md hover:bg-red-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5 -mt-1 inline-block"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <div class="flex">
+          <a
+            :href="`/view/survey/${model.slug}`"
+            target="_blank"
+            class="flex  px-3 items-center justify-center rounded-md border border-transparent text-sm text-indigo-500 focus:ring-indigo-500"
           >
-            <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Delete Survey
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+            View Survey
+          </a>
+          <button
+            v-if="route.params.id"
+            type="button"
+            @click="deleteSurvey()"
+            class="py-2 px-3 text-white bg-red-500 rounded-md hover:bg-red-600"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 -mt-1 inline-block"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Delete Survey
+          </button>
+        </div>
       </div>
     </template>
     <div v-if="surveyLoading" class="flex justify-center">Loading...</div>
@@ -47,16 +69,7 @@
               />
               <span
                 v-else
-                class="
-                  flex
-                  items-center
-                  justify-center
-                  h-12
-                  w-12
-                  rounded-full
-                  overflow-hidden
-                  bg-gray-100
-                "
+                class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -73,39 +86,12 @@
               </span>
               <button
                 type="button"
-                class="
-                  relative
-                  overflow-hidden
-                  ml-5
-                  bg-white
-                  py-2
-                  px-3
-                  border border-gray-300
-                  rounded-md
-                  shadow-sm
-                  text-sm
-                  leading-4
-                  font-medium
-                  text-gray-700
-                  hover:bg-gray-50
-                  focus:outline-none
-                  focus:ring-2
-                  focus:ring-offset-2
-                  focus:ring-indigo-500
-                "
+                class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <input
                   type="file"
                   @change="onImageChoose"
-                  class="
-                    absolute
-                    left-0
-                    top-0
-                    right-0
-                    bottom-0
-                    opacity-0
-                    cursor-pointer
-                  "
+                  class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer"
                 />
                 Change
               </button>
@@ -124,16 +110,7 @@
               id="title"
               v-model="model.title"
               autocomplete="survey_title"
-              class="
-                mt-1
-                focus:ring-indigo-500 focus:border-indigo-500
-                block
-                w-full
-                shadow-sm
-                sm:text-sm
-                border-gray-300
-                rounded-md
-              "
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
             />
           </div>
           <!--/ Title -->
@@ -150,16 +127,7 @@
                 rows="3"
                 v-model="model.description"
                 autocomplete="survey_description"
-                class="
-                  shadow-sm
-                  focus:ring-indigo-500 focus:border-indigo-500
-                  mt-1
-                  block
-                  w-full
-                  sm:text-sm
-                  border border-gray-300
-                  rounded-md
-                "
+                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                 placeholder="Describe your survey"
               />
             </div>
@@ -178,16 +146,7 @@
               name="expire_date"
               id="expire_date"
               v-model="model.expire_date"
-              class="
-                mt-1
-                focus:ring-indigo-500 focus:border-indigo-500
-                block
-                w-full
-                shadow-sm
-                sm:text-sm
-                border-gray-300
-                rounded-md
-              "
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
             />
           </div>
           <!--/ Expire Date -->
@@ -200,14 +159,7 @@
                 name="status"
                 type="checkbox"
                 v-model="model.status"
-                class="
-                  focus:ring-indigo-500
-                  h-4
-                  w-4
-                  text-indigo-600
-                  border-gray-300
-                  rounded
-                "
+                class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
               />
             </div>
             <div class="ml-3 text-sm">
@@ -220,7 +172,6 @@
         </div>
         <!--/ Survey Fields -->
 
-
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
           <h3 class="text-2xl font-semibold flex items-center justify-between">
             Questions
@@ -229,17 +180,7 @@
             <button
               type="button"
               @click="addQuestion()"
-              class="
-                flex
-                items-center
-                text-sm
-                py-1
-                px-4
-                rounded-sm
-                text-white
-                bg-gray-600
-                hover:bg-gray-700
-              "
+              class="flex items-center text-sm py-1 px-4 rounded-sm text-white bg-gray-600 hover:bg-gray-700"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -257,16 +198,10 @@
             </button>
             <!--/ Add new question -->
           </h3>
-          <div
-            v-if="!model.questions.length"
-            class="text-center text-gray-600"
-          >
+          <div v-if="!model.questions.length" class="text-center text-gray-600">
             You don't have any questions created
           </div>
-          <div
-            v-for="(question, index) in model.questions"
-            :key="question.id"
-          >
+          <div v-for="(question, index) in model.questions" :key="question.id">
             <QuestionEditor
               :question="question"
               :index="index"
@@ -280,24 +215,7 @@
         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
           <button
             type="submit"
-            class="
-              inline-flex
-              justify-center
-              py-2
-              px-4
-              border border-transparent
-              shadow-sm
-              text-sm
-              font-medium
-              rounded-md
-              text-white
-              bg-indigo-600
-              hover:bg-indigo-700
-              focus:outline-none
-              focus:ring-2
-              focus:ring-offset-2
-              focus:ring-indigo-500
-            "
+            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Save
           </button>
@@ -316,11 +234,11 @@ import { useRoute, useRouter } from "vue-router";
 import PageComponent from "../components/PageComponent.vue";
 import QuestionEditor from "../components/editor/QuestionEditor.vue";
 
-const router = useRouter()
+const router = useRouter();
 
 const route = useRoute();
 
-const surveyLoading = computed(() => store.state.currentSurvey.loading)
+const surveyLoading = computed(() => store.state.currentSurvey.loading);
 
 // Create empty survey
 let model = ref({
@@ -338,16 +256,16 @@ watch(
   (newVal, oldVal) => {
     model.value = {
       ...JSON.parse(JSON.stringify(newVal)),
-      status: newVal.status !== "draft",
+      status: !!newVal.status,
     };
   }
 );
 
 if (route.params.id) {
-  store.dispatch('getSurvey', route.params.id);
+  store.dispatch("getSurvey", route.params.id);
 }
 
-function onImageChoose (ev) {
+function onImageChoose(ev) {
   const file = ev.target.files[0];
 
   const reader = new FileReader();
@@ -374,9 +292,7 @@ function addQuestion(index) {
 }
 
 function deleteQuestion(question) {
-  model.value.questions = model.value.questions.filter(
-    (q) => q !== question
-  );
+  model.value.questions = model.value.questions.filter((q) => q !== question);
 }
 
 function questionChange(question) {
@@ -393,10 +309,10 @@ function questionChange(question) {
  */
 function saveSurvey() {
   store.dispatch("saveSurvey", model.value).then(({ data }) => {
-    store.commit('notify', {
-      type: 'success',
-      message: 'Survey was successfully updated'
-    })
+    store.commit("notify", {
+      type: "success",
+      message: "Survey was successfully updated",
+    });
     router.push({
       name: "SurveyView",
       params: { id: data.data.id },
